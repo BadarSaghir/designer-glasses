216ff6b452373c4690d741ce0a0163166c861031
Sardar Badar Saghir <dataentrybadar@gmail.com>
2023-02-24 16:38:49 +0500

    designer-glasses

---
+++ apps/api/src/app/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+
+import { AppService } from './app.service';
+// import {UpdateUserDto} from 'libs/dto/update-user.dto'
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getData() {
+    return this.appService.getData();
+  }
+}
