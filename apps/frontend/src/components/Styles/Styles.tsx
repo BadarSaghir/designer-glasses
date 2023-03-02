79d1ae194c0fd0c3514d626017f8b875dcefc90e
Sardar Badar Saghir <dataentrybadar@gmail.com>
2023-03-02 13:25:56 +0500

    added styles

--- apps/frontend/src/components/Styles/Styles.tsx
+++ apps/frontend/src/components/Styles/Styles.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 type Props = {
-  title:""
-}
+  title: '';
+};
 
 const Style = (props: Props) => {
   return (
-    <div>FeatureStyle</div>
-  )
-}
+    <div className="min-h-screen flex min-w-full ">
+      <div className="flex-grow  ">
+        <div className="h-full w-full  flex flex-col items-center">
+          <div className="flex-1  flex items-center justify-center w-full">
+            <div>A</div>
+          </div>
+          <div className="flex-1  flex items-center justify-center w-full">
+            <div>B</div>
+          </div>
+          <div className="flex-1 flex  items-center justify-center w-full">
+            <div>C</div>
+          </div>
+          <div className="flex-1 flex  items-center justify-center w-full">
+            <div>D</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Style
+export default Style;
