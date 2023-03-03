f921683f63a22c9ce92de75fd316fbab2014c094
Sardar Badar <dataentrybadar@gmail.com>
2023-03-03 14:33:23 +0500

    added class

--- apps/frontend/src/components/Header/Header.tsx
+++ apps/frontend/src/components/Header/Header.tsx
@@ -11,19 +11,15 @@ hreoImg:string
 
 const Header = ({navData,logo,imageText,hreoImg}: Props) => {
   return (
-    <header className='flex flex-col h-screen'>
+    <header className='flex flex-col h-screen '>
       <nav >
       <Navbar navitems={navData}  logo={logo}/>
       </nav>
-      <section id='Hero'  style={{
+      <section id='Hero'   style={{
       backgroundImage: `url(${hreoImg})`,
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: '100% 100%',
-    
 
-      backgroundPosition: '0% 0%'
 
-    }} className='flex-1 '>
+    }} className='flex-1 bgImg '>
       <Hero imageText={imageText} imgSrc={hreoImg}/>
       </section>
     </header>
