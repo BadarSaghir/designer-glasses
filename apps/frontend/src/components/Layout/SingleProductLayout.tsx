53280d43204151c051dadd8c30652896d57b4869
Sardar Badar <dataentrybadar@gmail.com>
2023-03-20 18:28:15 +0500

    added

--- apps/frontend/src/components/Layout/SingleProductLayout.tsx
+++ apps/frontend/src/components/Layout/SingleProductLayout.tsx
@@ -1,18 +1,19 @@
 import { Children, PropsWithChildren, useState } from 'react';
 import { ProductModel } from '@designer-glasses/libs/models/Products/products.interface';
-import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import CircleIcon from '@mui/icons-material/Circle';
 import CheckIcon from '@mui/icons-material/Check';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 function SingleProductLayout({
   children,
   product,
 }: PropsWithChildren & { product?: ProductModel }) {
-
-  const [selectedImage,setSelectedImage]= useState(0)
+  const [selectedImage, setSelectedImage] = useState(0);
+  const len = product?.images.length ||0;
   return (
     <div className="flex col  pt-6 pb-16 w-[100%]">
-      <div className="flex   flex-col justify-center p-4 md:justify-between md:flex-row w-[100%]  md:pl-28 md:p-16 md:pr-72">
+      <div className="flex   flex-col justify-center p-4 md:justify-around md:flex-row w-[100%]  md:pl-28 md:p-16 md:pr-72">
         <div className=" gap-4 flex-col  w-[100%] aspect-square md:p-8 flex items-center justify-center bg-white  md:w-[600px]">
           <div
             className="flex-1 w-[100%] h-[100%] flex flex-col justify-between"
@@ -37,28 +38,49 @@ function SingleProductLayout({
                 />
               </div>
             </div>{' '}
-            <div className="flex w-[100%] justify-center pb-4">
-              <div className="text-lg cursor-pointer font-bold text-secondaryMain border-solid border-tertiaryMain border-[2px] rounded-lg  text-opacity-70 px-3">
-                {'TRY ON'}
+            <div className="w-[100%] flex justify-between">
+              <div className='cursor-pointer'  onClick={() =>
+                    setSelectedImage((prevSelectedImage) => (prevSelectedImage - 1 + len) % len)
+
+                }>
+                {' '}
+                <ArrowBackIosNewIcon />{' '}
+              </div>
+              <div
+              className='cursor-pointer'
+                onClick={() =>
+                  setSelectedImage(
+                    (prevSelectedImage) => (prevSelectedImage + 1) % len
+                  )
+                }
+              >
+                <ArrowForwardIosIcon />
               </div>
             </div>
+            <div className="flex w-[100%] justify-center pb-4">
+              {/* <div className="text-lg cursor-pointer font-bold text-secondaryMain border-solid border-tertiaryMain border-[2px] rounded-lg  text-opacity-70 px-3">
+                {'TRY ON'}
+              </div> */}
+            </div>
           </div>
-          <div className='images flex justify-evenly gap-6 pb-3 w-[100%]'>
-            {product?.images.map((item,idx)=>{
-              if(idx<=3)
-               return  <div
-               onClick={(e)=>{setSelectedImage(idx)}}
-               key={idx}
-                 className="flex-1 cursor-pointer  w-24 h-24 flex flex-col justify-between"
-                 style={{
-                   backgroundImage: `url(${item})`,
-                   backgroundRepeat: 'no-repeat',
-                   backgroundSize: '100% 100%',
-                 }}
-               ></div>
-
+          <div className="images flex justify-evenly gap-6 pb-3 w-[100%]">
+            {product?.images.map((item, idx) => {
+              if (idx <= 3)
+                return (
+                  <div
+                    onClick={(e) => {
+                      setSelectedImage(idx);
+                    }}
+                    key={idx}
+                    className="flex-1 cursor-pointer  w-24 h-24 flex flex-col justify-between"
+                    style={{
+                      backgroundImage: `url(${item})`,
+                      backgroundRepeat: 'no-repeat',
+                      backgroundSize: '100% 100%',
+                    }}
+                  ></div>
+                );
             })}
-       
           </div>
         </div>
         <div className="flex  text-2xl justify-center items-center flex-col">
--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+// 
 import './styles.css';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from '../components/theme';
