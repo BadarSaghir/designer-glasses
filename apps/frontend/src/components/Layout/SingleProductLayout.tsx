ad4df52c2a10f5ab228264524acf589b6bd45602
Sardar Badar <dataentrybadar@gmail.com>
2023-03-20 16:48:15 +0500

    added single product

--- apps/frontend/src/components/Layout/SingleProductLayout.tsx
+++ apps/frontend/src/components/Layout/SingleProductLayout.tsx
@@ -7,7 +7,7 @@ function SingleProductLayout({
 }: PropsWithChildren & { product?: ProductModel }) {
   return (
     <div className="flex col  pt-6 pb-16 w-[100%]">
-      <div className="flex   flex-col justify-center p-4 md:justify-between md:flex-row w-[100%] md:p-16">
+      <div className="flex   flex-col justify-center p-4 md:justify-between md:flex-row w-[100%] md:p-16 md:pr-72">
         <div className="   w-[100%] aspect-square md:p-28  bg-white  md:w-[600px]">
           <div
             className="flex-1 w-[100%] h-[100%]"
@@ -17,7 +17,15 @@ function SingleProductLayout({
               backgroundSize: '100% 100%',
             }}
           >
-            {' '}
+            <div className="w-[100%] flex justify-between px-4 py-2">
+              <div
+                className="bg-danger-200 w-28 text-secondaryMain font-semibold text-opacity-70 text-center px-2 py-1 rounded-md
+              "
+              >
+                {product?.advertisment.type.toLocaleUpperCase()}
+              </div>
+              <div>s</div>
+            </div>{' '}
           </div>
         </div>
         <div className="flex  text-2xl justify-center items-center flex-col">
@@ -31,12 +39,28 @@ function SingleProductLayout({
             {product?.prices?.map((price, idx) => (idx == 0 ? price : ''))}
           </div>
           <div className="flex justify-center items-center gap-2">
-            <h1 className='text-base font-bold text-secondaryMain'>SIZES :</h1>{' '}
-            {product?.sku?.map((size, idx) => (<div className='font-extralight text-sm text-secondaryMain flex' key={idx}>{size.toLocaleUpperCase()}</div>))}
+            <h1 className="text-base font-bold text-secondaryMain">SIZES :</h1>{' '}
+            {product?.sku?.map((size, idx) => (
+              <div
+                className="font-extralight text-sm text-secondaryMain flex"
+                key={idx}
+              >
+                {size.toLocaleUpperCase()}
+              </div>
+            ))}
           </div>
           <div className="flex flex-col gap-2">
-            <h2 className='text-danger-700 text-lg font-bold mt-4 text-center'>PRICES INCLUDES</h2>{' '}
-            {product?.warranties?.map((ads, idx) => ( <div className=' text-base font-bold text-secondaryMain' key={idx}>{ads}</div>))}
+            <h2 className="text-danger-700 text-lg font-bold mt-4 text-center">
+              PRICES INCLUDES
+            </h2>{' '}
+            {product?.warranties?.map((ads, idx) => (
+              <div
+                className=" text-base font-bold text-secondaryMain"
+                key={idx}
+              >
+                {ads}
+              </div>
+            ))}
           </div>
           <div className="mt-8 flex gap-4 flex-col">
             {['SELECT LENSES', 'BUY FRAME ONLY', 'ADD TO CART'].map(
